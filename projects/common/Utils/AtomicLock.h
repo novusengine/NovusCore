19ffea80deca9246c22983ee20856065a1b21581
nixaj <nickiaj@live.dk>
2019-03-19 21:04:06 +0100

    [Common] Added AtomicLock.h which serves as a fake lock during development to catch instances of incorrect use of a specific type while running a system or other code in a multi threaded / parallel context. This was written by vblanco.

---
+++ projects/common/Utils/AtomicLock.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <atomic>
+#include <cassert>
+
+template<typename T>
+struct TypeLock 
+{
+    std::atomic<int> lock;
+};
+template<typename T>
+std::atomic<int>& GetWriters() 
+{
+    static TypeLock<T> typelock;
+    return typelock.lock;
+}
+template<typename T>
+std::atomic<int>& GetReaders() 
+{
+    static TypeLock<T> typelock;
+    return typelock.lock;
+}
+
+template<typename T>
+struct WriteLock 
+{
+    WriteLock() 
+    {
+        auto writercount = GetWriters<T>().fetch_add(1);
+        auto readercount = GetReaders<T>().load();
+
+        assert(writercount == 0);
+        assert(readercount == 0);
+    }
+    ~WriteLock() 
+    {
+        GetWriters<T>()--;
+    }
+};
+
+template<typename T>
+struct ReadLock 
+{
+    ReadLock() 
+    {
+        auto writercount = GetWriters<T>().load();
+        GetReaders<T>()++;
+
+        assert(writercount == 0);
+    }
+    ~ReadLock() 
+    {
+        GetReaders<T>()--;
+    }
+};
+
+#define TOKENPASTE(x, y) x ## y
+#define TOKENPASTE2(x, y) TOKENPASTE(x, y)
+
+#define LockWrite(comp) auto  TOKENPASTE2(wmutex, __LINE__) = WriteLock<comp>{};
+#define LockRead(comp) auto  TOKENPASTE2(rmutex, __LINE__) = ReadLock<comp>{};
