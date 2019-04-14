461ea59be60dd37948b92be844abfc1db87c20ad
nixaj <nickiaj@live.dk>
2019-04-15 00:58:31 +0200

    [Database Template] Updated template for dbcdata.sql

--- resources/Database Files/Templates/dbcdata.sql
+++ resources/Database Files/Templates/dbcdata.sql
@@ -4,8 +4,20 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping database structure for dbcdata
+
 CREATE DATABASE IF NOT EXISTS `dbcdata` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `dbcdata`;
+
+CREATE TABLE IF NOT EXISTS `map` (
+  `id` int(10) unsigned NOT NULL,
+  `internalName` varchar(255) NOT NULL,
+  `instanceType` int(10) unsigned NOT NULL,
+  `flags` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `expansion` int(10) unsigned NOT NULL,
+  `maxPlayers` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
