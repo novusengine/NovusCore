3a12ab0256c3113f4f356521b7c6b84a62bd4021
Nicki <nickiarpejensen@gmail.com>
2019-04-23 23:22:06 +0200

    DataStore implementation & usage in authserver.  (#19)
    * [Authserver] Replaced ByteBuffer with DataStore for HandleCommandRealmserverList & Added + Fixed Documentation
    
    [Common] Corrected DataStore to use little endian, Added PutF32, PutF64 and PutString
    
    * [Authserver] Renamed AUTH_GAMESERVER_LIST to AUTH_REALMSERVER_LIST. Refactored the code responsible for ensuring the client isn't spamming packets.

--- projects/authserver/Connections/AuthConnection.h
+++ projects/authserver/Connections/AuthConnection.h
@@ -33,11 +33,11 @@
 
 enum AuthCommand
 {
-    AUTH_CHALLENGE              = 0x00,
-    AUTH_PROOF                  = 0x01,
-    AUTH_RECONNECT_CHALLENGE    = 0x02,
-    AUTH_RECONNECT_PROOF        = 0x03,
-    AUTH_GAMESERVER_LIST        = 0x10,
+    AUTH_CHALLENGE = 0x00,
+    AUTH_PROOF = 0x01,
+    AUTH_RECONNECT_CHALLENGE = 0x02,
+    AUTH_RECONNECT_PROOF = 0x03,
+    AUTH_REALMSERVER_LIST = 0x10,
     /*
     TRANSFER_INITIATE           = 0x30,
     TRANSFER_DATA               = 0x31,
@@ -48,63 +48,63 @@ enum AuthCommand
 };
 enum AuthStatus
 {
-    STATUS_CHALLENGE                = 0,
-    STATUS_PROOF                    = 1,
-    STATUS_RECONNECT_PROOF          = 2,
-    STATUS_AUTHED                   = 3,
-    STATUS_WAITING_FOR_GAMESERVER   = 4,
-    STATUS_CLOSED                   = 5 
+    STATUS_CHALLENGE = 0,
+    STATUS_PROOF = 1,
+    STATUS_RECONNECT_PROOF = 2,
+    STATUS_AUTHED = 3,
+    STATUS_WAITING_FOR_REALMSERVER_LIST = 4,
+    STATUS_CLOSED = 5
 };
 
 enum AuthResult
 {
-    AUTH_SUCCESS                                  = 0x00,
-    AUTH_FAIL_BANNED                              = 0x03,
-    AUTH_FAIL_UNKNOWN_ACCOUNT                     = 0x04,
-    AUTH_FAIL_INCORRECT_PASSWORD                  = 0x05,
-    AUTH_FAIL_ALREADY_ONLINE                      = 0x06,
-    AUTH_FAIL_NO_TIME                             = 0x07,
-    AUTH_FAIL_DB_BUSY                             = 0x08,
-    AUTH_FAIL_VERSION_INVALID                     = 0x09,
-    AUTH_FAIL_VERSION_UPDATE                      = 0x0A,
-    AUTH_FAIL_INVALID_SERVER                      = 0x0B,
-    AUTH_FAIL_SUSPENDED                           = 0x0C,
-    AUTH_FAIL_FAIL_NOACCESS                       = 0x0D,
-    AUTH_SUCCESS_SURVEY                           = 0x0E,
-    AUTH_FAIL_PARENTCONTROL                       = 0x0F,
-    AUTH_FAIL_LOCKED_ENFORCED                     = 0x10,
-    AUTH_FAIL_TRIAL_ENDED                         = 0x11,
-    AUTH_FAIL_USE_BATTLENET                       = 0x12,
-    AUTH_FAIL_ANTI_INDULGENCE                     = 0x13,
-    AUTH_FAIL_EXPIRED                             = 0x14,
-    AUTH_FAIL_NO_GAME_ACCOUNT                     = 0x15,
-    AUTH_FAIL_CHARGEBACK                          = 0x16,
-    AUTH_FAIL_INTERNET_GAME_ROOM_WITHOUT_BNET     = 0x17,
-    AUTH_FAIL_GAME_ACCOUNT_LOCKED                 = 0x18,
-    AUTH_FAIL_UNLOCKABLE_LOCK                     = 0x19,
-    AUTH_FAIL_CONVERSION_REQUIRED                 = 0x20,
-    AUTH_FAIL_DISCONNECTED                        = 0xFF
+    AUTH_SUCCESS = 0x00,
+    AUTH_FAIL_BANNED = 0x03,
+    AUTH_FAIL_UNKNOWN_ACCOUNT = 0x04,
+    AUTH_FAIL_INCORRECT_PASSWORD = 0x05,
+    AUTH_FAIL_ALREADY_ONLINE = 0x06,
+    AUTH_FAIL_NO_TIME = 0x07,
+    AUTH_FAIL_DB_BUSY = 0x08,
+    AUTH_FAIL_VERSION_INVALID = 0x09,
+    AUTH_FAIL_VERSION_UPDATE = 0x0A,
+    AUTH_FAIL_INVALID_SERVER = 0x0B,
+    AUTH_FAIL_SUSPENDED = 0x0C,
+    AUTH_FAIL_FAIL_NOACCESS = 0x0D,
+    AUTH_SUCCESS_SURVEY = 0x0E,
+    AUTH_FAIL_PARENTCONTROL = 0x0F,
+    AUTH_FAIL_LOCKED_ENFORCED = 0x10,
+    AUTH_FAIL_TRIAL_ENDED = 0x11,
+    AUTH_FAIL_USE_BATTLENET = 0x12,
+    AUTH_FAIL_ANTI_INDULGENCE = 0x13,
+    AUTH_FAIL_EXPIRED = 0x14,
+    AUTH_FAIL_NO_GAME_ACCOUNT = 0x15,
+    AUTH_FAIL_CHARGEBACK = 0x16,
+    AUTH_FAIL_INTERNET_GAME_ROOM_WITHOUT_BNET = 0x17,
+    AUTH_FAIL_GAME_ACCOUNT_LOCKED = 0x18,
+    AUTH_FAIL_UNLOCKABLE_LOCK = 0x19,
+    AUTH_FAIL_CONVERSION_REQUIRED = 0x20,
+    AUTH_FAIL_DISCONNECTED = 0xFF
 };
 
 enum LoginResult
 {
-    LOGIN_OK                                     = 0x00,
-    LOGIN_FAILED                                 = 0x01,
-    LOGIN_FAILED2                                = 0x02,
-    LOGIN_BANNED                                 = 0x03,
-    LOGIN_UNKNOWN_ACCOUNT                        = 0x04,
-    LOGIN_UNKNOWN_ACCOUNT3                       = 0x05,
-    LOGIN_ALREADYONLINE                          = 0x06,
-    LOGIN_NOTIME                                 = 0x07,
-    LOGIN_DBBUSY                                 = 0x08,
-    LOGIN_BADVERSION                             = 0x09,
-    LOGIN_DOWNLOAD_FILE                          = 0x0A,
-    LOGIN_FAILED3                                = 0x0B,
-    LOGIN_SUSPENDED                              = 0x0C,
-    LOGIN_FAILED4                                = 0x0D,
-    LOGIN_CONNECTED                              = 0x0E,
-    LOGIN_PARENTALCONTROL                        = 0x0F,
-    LOGIN_LOCKED_ENFORCED                        = 0x10
+    LOGIN_OK = 0x00,
+    LOGIN_FAILED = 0x01,
+    LOGIN_FAILED2 = 0x02,
+    LOGIN_BANNED = 0x03,
+    LOGIN_UNKNOWN_ACCOUNT = 0x04,
+    LOGIN_UNKNOWN_ACCOUNT3 = 0x05,
+    LOGIN_ALREADYONLINE = 0x06,
+    LOGIN_NOTIME = 0x07,
+    LOGIN_DBBUSY = 0x08,
+    LOGIN_BADVERSION = 0x09,
+    LOGIN_DOWNLOAD_FILE = 0x0A,
+    LOGIN_FAILED3 = 0x0B,
+    LOGIN_SUSPENDED = 0x0C,
+    LOGIN_FAILED4 = 0x0D,
+    LOGIN_CONNECTED = 0x0E,
+    LOGIN_PARENTALCONTROL = 0x0F,
+    LOGIN_LOCKED_ENFORCED = 0x10
 };
 
 #pragma pack(push, 1)
@@ -114,7 +114,7 @@ struct AuthMessageHandler
     AuthStatus status;
     size_t packetSize;
     u8 maxPacketsPerRead;
-    bool (AuthConnection::*handler)();
+    bool (AuthConnection::* handler)();
 };
 #pragma pack(pop)
 
@@ -123,8 +123,8 @@ class AuthConnection : public Common::BaseSocket
 public:
     static robin_hood::unordered_map<u8, AuthMessageHandler> InitMessageHandlers();
 
-    AuthConnection(asio::ip::tcp::socket* socket) : Common::BaseSocket(socket), _status(STATUS_CHALLENGE), username(), packetsReadThisRead(17)
-    { 
+    AuthConnection(asio::ip::tcp::socket* socket) : Common::BaseSocket(socket), _status(STATUS_CHALLENGE), username()
+    {
         N.Hex2BN("894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7");
         g.SetUInt32(7);
 
@@ -141,7 +141,7 @@ public:
     void HandleCommandReconnectChallengeCallback(amy::result_set& results);
     bool HandleCommandProof();
     bool HandleCommandReconnectProof();
-    bool HandleCommandGameServerList();
+    bool HandleCommandRealmserverList();
 
     BigNumber N, s, g, v;
     BigNumber b, B;
@@ -152,17 +152,13 @@ public:
     std::string username;
     u32 accountGuid;
 
-    void ResetPacketsReadThisRead() 
-    { 
-        for (u8 i = 0; i < 4; ++i)
+    void ResetPacketsReadThisRead()
+    {
+        for (u8 i = 0; i < 4; i++)
         {
             packetsReadThisRead[i] = 0;
-
-            if (i == 3)
-            {
-                packetsReadThisRead[AUTH_GAMESERVER_LIST] = 0;
-            }
         }
     }
-    std::vector<u8> packetsReadThisRead;
+    u8 packetsReadOfType = 0;
+    u8 packetsReadThisRead[4];
 };
