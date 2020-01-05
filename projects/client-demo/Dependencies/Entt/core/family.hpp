099ca652b319e7f05b341cf6411972eda80256ed
NixAJ <nickiarpejensen@gmail.com>
2020-01-05 18:51:54 +0100

    Added Entt 3.2.2 to Client-Demo

---
+++ projects/client-demo/Dependencies/Entt/core/family.hpp
@@ -0,0 +1,37 @@
+#ifndef ENTT_CORE_FAMILY_HPP
+#define ENTT_CORE_FAMILY_HPP
+
+
+#include <type_traits>
+#include "../config/config.h"
+
+
+namespace entt {
+
+
+/**
+ * @brief Dynamic identifier generator.
+ *
+ * Utility class template that can be used to assign unique identifiers to types
+ * at runtime. Use different specializations to create separate sets of
+ * identifiers.
+ */
+template<typename...>
+class family {
+    inline static ENTT_MAYBE_ATOMIC(ENTT_ID_TYPE) identifier{};
+
+public:
+    /*! @brief Unsigned integer type. */
+    using family_type = ENTT_ID_TYPE;
+
+    /*! @brief Statically generated unique identifier for the given type. */
+    template<typename... Type>
+    // at the time I'm writing, clang crashes during compilation if auto is used instead of family_type
+    inline static const family_type type = identifier++;
+};
+
+
+}
+
+
+#endif // ENTT_CORE_FAMILY_HPP
